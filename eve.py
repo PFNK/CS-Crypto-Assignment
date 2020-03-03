d83a5c4d7eabc6b0797475fe86bd685183bde2fc
PFNK <43795837+PFNK@users.noreply.github.com>
2020-03-03 16:04:10 +0000

    fixed it

--- eve.py
+++ eve.py
@@ -1,37 +1,61 @@
 import sys, os, time
-sys.path.append('/afs/inf.ed.ac.uk/group/teaching/compsec/cw2/mitm')
 from common import *
 from const import *
 
-b_dir = Template('/tmp/$usr/').substitute(usr = getpass.getuser())
-os.rename(b_dir + BUFFER_FILE_NAME, b_dir + 'buffer_original')
+os.rename(BUFFER_DIR + '/' + BUFFER_FILE_NAME, BUFFER_DIR + '/' + 'buffer_original')
 chat_type = sys.argv[1]
 dialog = Dialog('print')
 
 socket_to_bob, aes_to_bob = setup('alice', BUFFER_DIR, 'buffer_original')
 socket_to_alice, aes_to_alice = setup('bob', BUFFER_DIR, BUFFER_FILE_NAME)
 
-if chat_type == '--custom':
-    dialog.prompt('Please input what you would like Alice to say to Bob: ')
-    to_send_to_alice = input()
-    encrypt_and_send(to_send_to_alice, aes_to_bob, socket_to_bob)
+# to_send_to_bob = receive_and_decrypt(aes_to_bob, socket_to_bob)
+# to_send_to_alice = receive_and_decrypt(aes_to_alice, socket_to_alice)
 
-    dialog.prompt('Please input what you would like Bob to say to Alice: ')
+if chat_type == '--custom':
+    #receive true message from alice to bob
+    to_send_to_bob = receive_and_decrypt(aes_to_alice, socket_to_alice)
+    #get the custom message from user
+    dialog.prompt('Please input what you would like Alice to say to Bob: ')
     to_send_to_bob = input()
-    encrypt_and_send(to_send_to_bob, aes_to_alice, socket_to_alice)
+    #send custom message to bob
+    encrypt_and_send(to_send_to_bob, aes_to_bob, socket_to_bob)
+
+    #receive true message from bob to alice
+    to_send_to_alice = receive_and_decrypt(aes_to_bob, socket_to_bob)
+    #get the custom message from user
+    dialog.prompt('Please input what you would like Bob to say to Alice: ')
+    to_send_to_alice = input()
+    #send custom message to alice
+    encrypt_and_send(to_send_to_alice, aes_to_alice, socket_to_alice)
 
 
 elif chat_type == '--break-heart':
-    to_send_to_alice = BAD_MSG['alice']
-    encrypt_and_send(to_send_to_alice, aes_to_bob, socket_to_bob)
+    #receive true message from alice to bob
+    to_send_to_bob = receive_and_decrypt(aes_to_alice, socket_to_alice)
+    #get the alice's bad message
+    to_send_to_bob = BAD_MSG['alice']
+    #send alice's bad message to bob
+    encrypt_and_send(to_send_to_bob, aes_to_bob, socket_to_bob)
 
-    to_send_to_bob = BAD_MSG['bob']
-    encrypt_and_send(to_send_to_bob, aes_to_alice, socket_to_alice)
+    #receive true message from bob to alice
+    to_send_to_alice = receive_and_decrypt(aes_to_bob, socket_to_bob)
+    #get the bob's bad message
+    to_send_to_alice = BAD_MSG['bob']
+    #send bob's bad message to alice
+    encrypt_and_send(to_send_to_alice, aes_to_alice, socket_to_alice)
 
 else:
-    to_send_to_alice = receive_and_decrypt(aes_to_alice, socket_to_alice)
-    encrypt_and_send(to_send_to_alice, aes_to_bob, socket_to_bob)
+    #receive true message from alice to bob
+    to_send_to_bob = receive_and_decrypt(aes_to_alice, socket_to_alice)
+    #sent the true message from alice to bob
+    encrypt_and_send(to_send_to_bob, aes_to_bob, socket_to_bob)
 
-    to_send_to_bob = receive_and_decrypt(aes_to_bob, socket_to_bob)
-    encrypt_and_send(to_send_to_bob, aes_to_alice, socket_to_alice)
+    #receive true message from bob to alice
+    to_send_to_alice = receive_and_decrypt(aes_to_bob, socket_to_bob)
+    #sent the true message from bob to alice
+    encrypt_and_send(to_send_to_alice, aes_to_alice, socket_to_alice)
 
+
+tear_down(socket_to_bob, BUFFER_DIR, 'buffer_original')
+tear_down(socket_to_alice, BUFFER_DIR, BUFFER_FILE_NAME)
